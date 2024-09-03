e67dac86a0a988ef436350d409817058de7978aa
YskHmd <yusuke7075@gmail.com>
2024-09-03 15:49:28 +0900

    Helloworld完

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import helloworldfanction, HelloWorldClass
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('helloworld/', helloworldfanction),
     path('helloworld2/', HelloWorldClass.as_view()),
+    path('app/', include('Helloworldapp.urls')),
 ]
