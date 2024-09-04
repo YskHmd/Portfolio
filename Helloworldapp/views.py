012fd9ea37b94d2f72cb81ff7ee273c4fa13b69e
YskHmd <yusuke7075@gmail.com>
2024-09-04 14:44:45 +0900

    a

--- Helloworldapp/views.py
+++ Helloworldapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def helloworldappview(request):
-    return HttpResponse('aaaaaa')
+    return HttpResponse('you seeing apps view ')
